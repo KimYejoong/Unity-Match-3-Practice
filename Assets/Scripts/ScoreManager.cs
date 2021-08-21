6a5b5cffb5363d6d7e9309f29408423a6a2157f4
KimYejoong <88814035+KimYejoong@users.noreply.github.com>
2021-08-21 16:07:20 +0900

    Inital Commit

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    Text scoreValue;
+    Text comboText;
+
+    int currentScore = 0;
+
+    private void Start()
+    {
+        ResetPoint();        
+    }
+
+    public void AddPoint(int value)
+    {
+        currentScore += value;
+        scoreValue.text = currentScore.ToString();
+    }
+
+    public void ResetPoint()
+    {
+        currentScore = 0;
+        scoreValue.text = currentScore.ToString();
+    }
+
+}
---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+
+    [SerializeField]
+    Slider timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetTime(float timeElapsed, float timeMax)
+    {
+        timer.value = timeElapsed / timeMax;
+    }
+}
